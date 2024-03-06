f92df35111fd10afa7590a0d750baf4c00650e61
ayoub-kplr <ayoub.hebbaj@kplr.fr>
2024-03-06 12:53:25 +0100

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,167 @@
 import 'package:flutter/material.dart';
-import 'package:kplr_app/screens/home.dart';
+import 'package:kplr_app/screens/unified.dart';
 
-void main() => runApp(const MyApp());
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Map<String, dynamic>> _listItem = [
+    {
+      "image": 'assets/images/1.png',
+      "title": "Movie Recommendation",
+      "api": "https://ai.kplr.fr:444"
+    },
+    {
+      "image": 'assets/images/2.png',
+      "title": "Cars Detection",
+      "api": "https://ai.kplr.fr:445"
+    },
+    {
+      "image": 'assets/images/3.png',
+      "title": "Behavior detector",
+      "api": "https://ai.kplr.fr:446"
+    },
+    {
+      "image": 'assets/images/4.png',
+      "title": "ChatGPT",
+      "api": "https://ai.kplr.fr:447"
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Material App',
-      home: Home(),
+    return Scaffold(
+      backgroundColor: Colors.grey[600],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(Icons.menu),
+        title: Text("Home"),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Container(
+              width: 36,
+              height: 30,
+              decoration: BoxDecoration(
+                  color: Colors.grey[800],
+                  borderRadius: BorderRadius.circular(10)),
+              child: Center(child: Text("0")),
+            ),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                height: 250,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/one.png'),
+                        fit: BoxFit.cover)),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient:
+                          LinearGradient(begin: Alignment.bottomRight, colors: [
+                        Colors.black.withOpacity(.4),
+                        Colors.black.withOpacity(.2),
+                      ])),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "KPLR use cases",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                  child: GridView.count(
+                crossAxisCount: 1,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                children: _listItem
+                    .map((item) => GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (_) {
+                                return Unified(
+                                    title: item["title"], api: item["api"]);
+                              },
+                            ),
+                          );
+                        },
+                        child: Card(
+                          color: Colors.transparent,
+                          elevation: 0,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                image: DecorationImage(
+                                    image: AssetImage(item["image"]),
+                                    fit: BoxFit.cover)),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  gradient: LinearGradient(
+                                      begin: Alignment.bottomRight,
+                                      colors: [
+                                        Colors.black.withOpacity(.4),
+                                        Colors.black.withOpacity(.2),
+                                      ])),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  Text(
+                                    item["title"],
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 35,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )))
+                    .toList(),
+              ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
